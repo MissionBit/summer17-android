03f1a24722a0c5ecee489364297551f9de7e53a9
Jaime <ixjaime@gmail.com>
2017-06-26 13:57:20 -0700

    added a MenuState class

---
+++ SheepDash/core/src/com/missionbit/game/MenuState.java
@@ -0,0 +1,17 @@
+package com.missionbit.game;
+
+import com.badlogic.gdx.graphics.Texture;
+
+/**
+ * Created by missionbit on 6/26/17.
+ */
+
+public class MenuState {
+    private Texture startButton;
+    private Texture sceneSelect;
+    private Texture charSelect;
+
+
+    public MenuState() {
+    }
+}
