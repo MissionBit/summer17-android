faf0c6266a00d2ab2b744fddf848202a52b53ba5
jasandrob <jasonroberts671@gmail.com>
2017-06-27 09:21:43 -0700

    first commit with start screen

--- NoObjections/android/src/com/missionbit/game/AndroidLauncher.java
+++ NoObjections/android/src/com/missionbit/game/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.missionbit.game.NoObjectionGame;
+import com.missionbit.game.FlappyDemo;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new NoObjectionGame(), config);
+		initialize(new FlappyDemo(), config);
 	}
 }
