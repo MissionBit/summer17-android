8505913785b567bc90bc2e4605ca0358f425514c
ixJaime <ixjaime@gmail.com>
2017-07-05 10:32:35 -0700

    Updated the charselect and game state

--- SheepDash/core/src/com/missionbit/game/GameTutorial.java
+++ SheepDash/core/src/com/missionbit/game/GameTutorial.java
@@ -3,7 +3,6 @@ package com.missionbit.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.missionbit.game.states.GameStateManager;
 import com.missionbit.game.states.MenuState;
@@ -20,11 +19,12 @@ public class GameTutorial extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		gsm = new GameStateManager();
 		gsm.push(new MenuState(gsm));
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		gsm.update(Gdx.graphics.getDeltaTime());
 		gsm.render(batch);
 	}
--- SheepDash/core/src/com/missionbit/game/states/CharacterState.java
+++ SheepDash/core/src/com/missionbit/game/states/CharacterState.java
@@ -1,8 +1,61 @@
 package com.missionbit.game.states;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.missionbit.game.GameTutorial;
+
 /**
  * Created by missionbit on 6/26/17.
  */
 
-public class CharacterState {
+public class CharacterState extends State {
+    private Texture bg;
+    private Texture sheep;
+    private Texture cow;
+    private Texture pig;
+    private Texture chick;
+    private Texture bunny;
+
+    public CharacterState(GameStateManager gsm) {
+        super(gsm);
+        bg = new Texture("CharScreen.jpg");
+        sheep = new Texture("SheepButton.png");
+        cow = new Texture("CowButton.png");
+        pig = new Texture("PigButton.png");
+        chick = new Texture("ChickButton.png");
+        bunny = new Texture("BunnyButton.png");
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(bg, 0, 0, GameTutorial.WIDTH, GameTutorial.HEIGHT);
+        sb.draw(sheep, 100, 245, 100, 100);
+        sb.draw(cow, 250, 245, 100, 100);
+        sb.draw(pig, 400, 245, 100, 100);
+        sb.draw(chick, 175, 95, 100, 100);
+        sb.draw(bunny, 325, 95, 100, 100);
+        sb.end();
+    }
+
+    @Override
+    public void dispose() {
+        bg.dispose();
+        sheep.dispose();
+        cow.dispose();
+        pig.dispose();
+        chick.dispose();
+        bunny.dispose();
+    }
+
 }
