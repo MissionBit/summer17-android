8d3413173665459cb866d776750653566c304afd
shaikh7860 <ali.shaikh@hotmail.com>
2017-07-11 13:46:20 -0700

    Implememnted restart button

--- NoObjections/core/src/com/missionbit/game/NoObjectionGame.java
+++ NoObjections/core/src/com/missionbit/game/NoObjectionGame.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.graphics.GL20;
 //import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.missionbit.game.screens.PlayScreen;
+import com.missionbit.game.states.EndState;
+import com.missionbit.game.states.GameStateManager;
+import com.missionbit.game.states.MenuState;
 //import com.missionbit.game.states.EndState;
 //import com.missionbit.game.states.GameStateManager;
 //import com.missionbit.game.states.MenuState;
@@ -19,21 +22,23 @@ public class NoObjectionGame extends Game {
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 480;
 	public static final String TITLE = "Flummox";
-	//private GameStateManager gsm;
+	private GameStateManager gsm;
 	public SpriteBatch batch;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		setScreen(new PlayScreen(this));
-//		gsm = new GameStateManager();
-//		gsm.push(new MenuState(gsm));
+//		setScreen(new PlayScreen(this));
+		gsm = new GameStateManager();
+		gsm.push(new EndState(gsm));
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 	}
 
 	@Override
 	public void render () {
-		super.render();
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        gsm.update(Gdx.graphics.getDeltaTime());
+        gsm.render(batch);
 	}
 	
 	@Override
--- NoObjections/core/src/com/missionbit/game/states/EndState.java
+++ NoObjections/core/src/com/missionbit/game/states/EndState.java
@@ -4,9 +4,17 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.missionbit.game.NoObjectionGame;
 
 
 /**
@@ -19,12 +27,36 @@ public class EndState extends State {
     private Texture instructBtn;
     private TextButton tb;
     private TextButton.TextButtonStyle textButtonStyle;
+    private Stage stage;
+    private Texture myTexture;
+    private TextureRegion myTextureRegion;
+    private TextureRegionDrawable myTexRegionDrawable;
+    private ImageButton button;
 
-    public EndState(GameStateManager gsm) {
+
+    public EndState(final GameStateManager gsm) {
         super(gsm);
         background = new Texture("pixil-layer-Background.png");
-        textButtonStyle = new TextButton.TextButtonStyle();
-        tb = new TextButton("Restart", textButtonStyle);
+        //textButtonStyle = new TextButton.TextButtonStyle();
+       // tb = new TextButton("Restart", textButtonStyle);
+
+        myTexture = new Texture(Gdx.files.internal("Restart_button.png")); //put the image you want for your button here
+        myTextureRegion = new TextureRegion(myTexture);
+        myTexRegionDrawable = new TextureRegionDrawable(myTextureRegion);
+        button = new ImageButton(myTexRegionDrawable); //Set the button up
+        stage = new Stage(new StretchViewport(NoObjectionGame.WIDTH, NoObjectionGame.HEIGHT));
+        stage.addActor(button); //Add the button to the stage to perform rendering and take input.
+
+        button.setBounds(275, 150 , 250, 200); //x, y, width (of button), height (of button) //x, y, width (of button), height (of button)
+
+        Gdx.input.setInputProcessor(stage); //Start taking input from the ui
+        button.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                gsm.push(new MenuState(gsm));
+            }
+        });
+
 
     }
 
@@ -44,18 +76,24 @@ public class EndState extends State {
 
     @Override
     public void update(float dt) {
+        handleInput();
 
     }
 
     @Override
     public void render(SpriteBatch sb) {
         sb.begin();
-        sb.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+       // sb.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        stage.act(Gdx.graphics.getDeltaTime()); //Perform ui logic
+        stage.draw(); //Draw the ui
+
         sb.end();
     }
 
     public void dispose() {
+
         background.dispose();
+        stage.dispose();
     }
 
 }
