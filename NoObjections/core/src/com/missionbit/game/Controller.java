69146fe78e504bca7b2772ed66615f004fd98e0b
Sebastiaan Leddy <seleddy@s.sfusd.edu>
2017-07-12 09:12:22 -0700

    added a somewhat of a controler

---
+++ NoObjections/core/src/com/missionbit/game/Controller.java
@@ -0,0 +1,136 @@
+package com.missionbit.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+/**
+ * Created by missionbit on 7/11/17.
+ */
+
+public class Controller {
+    private SpriteBatch batch;
+    Viewport viewport;
+    Stage stage;
+    boolean upPressed, downPressed, leftPressed, rightPressed;
+    OrthographicCamera cam;
+
+    public Controller() {
+        cam = new OrthographicCamera();
+        viewport = new FitViewport(800, 400, cam);
+        stage = new Stage(viewport, NoObjectionGame.batch);
+        Gdx.input.setInputProcessor(stage);
+
+        Table table = new Table();
+        table.left().bottom();
+
+        Image upImg = new Image(new Texture("upArrow.png"));
+        upImg.setSize(50, 50);
+
+        upImg.addListener(new InputListener() {
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                leftPressed = true;
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                leftPressed = true;
+            }
+        });
+        Image downImg = new Image(new Texture("downArrow.png"));
+        downImg.setSize(50, 50);
+        downImg.addListener(new InputListener() {
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                downPressed = true;
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                downPressed = false;
+            }
+        });
+        Image leftImg = new Image(new Texture("leftArrow.png"));
+        leftImg.setSize(50, 50);
+        leftImg.addListener(new InputListener() {
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                leftPressed = true;
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                leftPressed = false;
+            }
+        });
+        Image rightImg = new Image(new Texture("rightArrow.png"));
+        rightImg.setSize(50, 50);
+        rightImg.addListener(new InputListener() {
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                rightPressed = true;
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                rightPressed = false;
+            }
+        });
+
+        table.add();
+        table.add(upImg).size(upImg.getWidth(), upImg.getHeight());
+        table.add();
+        table.row().pad(5, 5, 5, 5);
+        table.add(upImg).size(upImg.getWidth(), upImg.getHeight());
+        table.add(rightImg).size(rightImg.getWidth(), rightImg.getHeight());
+        table.row().padBottom(5);
+        table.add();
+        table.add(downImg).size(downImg.getWidth(), downImg.getHeight());
+        table.add();
+
+        stage.addActor(table);
+
+    }
+
+    public void draw() {
+        stage.draw();
+    }
+
+    public boolean isUpPressed() {
+        return upPressed;
+    }
+
+    public boolean isDownPressed() {
+        return downPressed;
+    }
+
+    public boolean isLeftPressed() {
+        return leftPressed;
+    }
+
+    public boolean isRightPressed() {
+        return rightPressed;
+    }
+
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
+}
--- NoObjections/core/src/com/missionbit/game/NoObjectionGame.java
+++ NoObjections/core/src/com/missionbit/game/NoObjectionGame.java
@@ -5,6 +5,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.missionbit.game.sprites.Hero;
 import com.missionbit.game.states.EndState;
 import com.missionbit.game.states.GameStateManager;
 import com.missionbit.game.states.MenuState;
@@ -12,34 +21,73 @@ import com.missionbit.game.states.PlayState;
 
 public class NoObjectionGame extends ApplicationAdapter {
 
-	public static final int WIDTH = 800;
-	public static final int HEIGHT = 480;
-	public static final String TITLE = "Flummox";
-	private GameStateManager gsm;
-	private SpriteBatch batch;
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 480;
+    public static final float PPM = 100;
+    public static final String TITLE = "Flummox";
+    public GameStateManager gsm;
+    public static SpriteBatch batch;
+    World world;
+    public Viewport viewport;
+    public Controller controller;
+    public Body hero;
 
-	@Override
-	public void create () {
 
-		batch = new SpriteBatch();
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        gsm = new GameStateManager();
+        gsm.push(new MenuState(gsm));
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        controller = new Controller();
+        viewport = new FitViewport(WIDTH, HEIGHT);
+        world = new World(new Vector2(0, -10), true);
 
-		gsm = new GameStateManager();
-		gsm.push(new MenuState(gsm));
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-	}
+    }
 
-	@Override
-	public void render () {
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        viewport.update(width, height);
+        controller.resize(width, height);
+    }
 
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    public void handleInput() {
+        if (controller.isRightPressed())
+            hero.setLinearVelocity(new Vector2(1, hero.getLinearVelocity().y));
+        else if (controller.isLeftPressed())
+            hero.setLinearVelocity(new Vector2(-1, hero.getLinearVelocity().y));
+        else
+            hero.setLinearVelocity(new Vector2(0, hero.getLinearVelocity().y));
+        if (controller.isUpPressed() && hero.getLinearVelocity().y == 0)
+            hero.applyLinearImpulse(new Vector2(0, 5f), hero.getWorldCenter(), true);
+
+    }
+
+
+    @Override
+    public void render() {
+
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         gsm.update(Gdx.graphics.getDeltaTime());
         gsm.render(batch);
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
+        controller.draw();
+    }
 
-	}
+    @Override
+    public void dispose() {
+        batch.dispose();
+
+    }
+
+    public void createPlayer(){
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(viewport.getWorldWidth() / 2, 80 / PPM);
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        hero = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        hero.createFixture(fdef);
+    }
 }
