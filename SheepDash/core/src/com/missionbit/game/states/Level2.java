e40cdd72d5a5a0b36c37300345990d06b5b5dc14
Melinda <contact.melindali@gmail.com>
2017-07-12 09:30:09 -0700

    level three with obstacles

--- SheepDash/core/src/com/missionbit/game/states/Level2.java
+++ SheepDash/core/src/com/missionbit/game/states/Level2.java
@@ -1,10 +1,34 @@
 package com.missionbit.game.states;
 
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 /**
  * Created by missionbit on 6/28/17.
  */
 
-public class Level2{
+public class Level2 extends State {
+    public Level2(GameStateManager gsm) {
+        super(gsm);
+    }
 
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
