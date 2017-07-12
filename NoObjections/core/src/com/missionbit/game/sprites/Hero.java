58a0ce9291c7c6eb1b61c3061d458bf01e8f6a39
jasandrob <jasonroberts671@gmail.com>
2017-07-12 11:05:04 -0700

    walking done

--- NoObjections/core/src/com/missionbit/game/sprites/Hero.java
+++ NoObjections/core/src/com/missionbit/game/sprites/Hero.java
@@ -12,10 +12,10 @@ import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.missionbit.game.NoObjectionGame;
 import com.missionbit.game.screens.PlayScreen;
-
-import java.awt.Rectangle;
+import com.badlogic.gdx.graphics.g2d.Animation;
 
 
 /**
@@ -99,21 +99,86 @@ public class Hero extends Sprite {
 ////    public boolean climbing() {
 ////        return true;
 ////    }
+    public enum State { FALLING, JUMPING, STANDING, RUNNING};
+    public State currentState;
+    public State previousState;
     public World world;
     public Body b2body;
     private TextureRegion heroStand;
+    private Animation heroRun;
+    private Animation heroClimb;
+    private float stateTimer;
+    private boolean runningRight;
 
     public Hero(World world, PlayScreen screen){
         super(screen.getAtlas().findRegion("dudeRun2"));
         this.world = world;
+        currentState = State.STANDING;
+        previousState = State.STANDING;
+        stateTimer = 0;
+        runningRight = true;
+
+        Array<TextureRegion> frames = new Array<TextureRegion>();
+        for(int i = 1; i <5; i++) {
+            frames.add(new TextureRegion(getTexture(), i * 46, 0, 35, 45));
+        }
+        heroRun = new Animation(0.1f, frames);
+
+        //TODO: add climbing images to image pack
+
+        frames.clear();
+
         defineHero();
+
         heroStand = new TextureRegion(getTexture(), 0, 0, 40, 45);
-        setBounds(0, 0, 40/ NoObjectionGame.PPM, 45/ NoObjectionGame.PPM);
+        setBounds(0, 0, 40 / NoObjectionGame.PPM, 45/ NoObjectionGame.PPM);
         setRegion(heroStand);
+
     }
 
     public void update(float dt){
         setPosition(b2body.getPosition().x - getWidth()/2, b2body.getPosition().y - getHeight()/2);
+        setRegion(getFrame(dt));
+    }
+
+    public TextureRegion getFrame(float dt){
+        currentState = getState();
+
+        TextureRegion region;
+        switch(currentState){
+            //TODO: case climbing
+            case RUNNING:
+                region = (TextureRegion) heroRun.getKeyFrame(stateTimer, true);
+                break;
+            case STANDING:
+                default:
+                    region = heroStand;
+                    break;
+        }
+
+        if((b2body.getLinearVelocity().x < 0 || !runningRight) && !region.isFlipX()) {
+            region.flip(true, false);
+            runningRight = false;
+        }else if((b2body.getLinearVelocity().x > 0 || runningRight) && region.isFlipX()){
+            region.flip(true, false);
+            runningRight = true;
+        }
+        stateTimer = currentState == previousState ? stateTimer + dt :0;
+        previousState = currentState;
+        return region;
+    }
+
+    public State getState(){
+//        if(b2body.getLinearVelocity().y > 0){
+//            return State.JUMPING;
+//        }
+
+        //TODO: need to code climbing
+        if(b2body.getLinearVelocity().x != 0){
+            return State.RUNNING;
+        }else{
+            return State.STANDING;
+        }
     }
 
     public void defineHero(){
@@ -122,15 +187,9 @@ public class Hero extends Sprite {
         bdef.type = BodyDef.BodyType.DynamicBody;
         b2body = world.createBody(bdef);
 
-//        FixtureDef fdef = new FixtureDef();
-//        CircleShape shape = new CircleShape();
-//        shape.setRadius(15/ NoObjectionGame.PPM);
-//
-//        fdef.shape = shape;
-
         FixtureDef fdef = new FixtureDef();
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox(4 / NoObjectionGame.PPM, 18 / NoObjectionGame.PPM);
+        shape.setAsBox(5 / NoObjectionGame.PPM, 18 / NoObjectionGame.PPM);
 
         fdef.shape = shape;
         b2body.createFixture(fdef);
